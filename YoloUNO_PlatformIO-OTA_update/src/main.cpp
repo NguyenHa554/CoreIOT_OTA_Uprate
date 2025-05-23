649211bf9103765dd7a57baad84dabc7f91e8e17
unknown <nguyen.hadtscvd554@hcmut.edu.vn>
2025-05-23 13:44:11 +0700

    testing

--- YoloUNO_PlatformIO-OTA_update/src/main.cpp
+++ YoloUNO_PlatformIO-OTA_update/src/main.cpp
@@ -1,203 +1,73 @@
-#define CONFIG_THINGSBOARD_ENABLE_DEBUG false
-#include <WiFi.h>
-#include <Arduino_MQTT_Client.h>
-#include <OTA_Firmware_Update.h>
-#include <ThingsBoard.h>
-#include <Shared_Attribute_Update.h>
-#include <Attribute_Request.h>
-#include <Espressif_Updater.h>
+#include <SPI.h>
 #include <MFRC522.h>
 
+// #define SS_PIN    5   // D5
+// #define RST_PIN   4   // D4
 
+// MFRC522 rfid(SS_PIN, RST_PIN);
+// unsigned long lastUIDTime = 0;
+// String lastUID = "";
 
+// void setup() {
+//   Serial.begin(115200);
+//   SPI.begin();               // SCK, MISO, MOSI auto map
+//   rfid.PCD_Init();           // Init RFID
+//   Serial.println("Chạm thẻ RFID...");
+// }
 
-//Shared Attributes Configuration
-constexpr uint8_t MAX_ATTRIBUTES = 2U; //
-constexpr std::array<const char*, MAX_ATTRIBUTES> 
-SHARED_ATTRIBUTES = 
-{
-  "POWER",
-  "ledState"
-};
+// void loop() {
+//   // Kiểm tra thẻ mới
+//   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+//     return;
+//   }
 
-constexpr int16_t TELEMETRY_SEND_INTERVAL = 5000U;
-uint32_t previousTelemetrySend; 
-// Firmware title and version used to compare with remote version, to check if an update is needed.
-// Title needs to be the same and version needs to be different --> downgrading is possible
-constexpr char CURRENT_FIRMWARE_TITLE[] = "OTA";
-constexpr char CURRENT_FIRMWARE_VERSION[] = "1.1";
-// Maximum amount of retries we attempt to download each firmware chunck over MQTT
-constexpr uint8_t FIRMWARE_FAILURE_RETRIES = 12U;
-// Size of each firmware chunck downloaded over MQTT,
-// increased packet size, might increase download speed
-constexpr uint16_t FIRMWARE_PACKET_SIZE = 4096U;
+//   // Đọc UID
+//   String uidStr = "";
+//   for (byte i = 0; i < rfid.uid.size; i++) {
+//     uidStr += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+//     uidStr += String(rfid.uid.uidByte[i], HEX);
+//   }
+//   uidStr.toUpperCase();
 
-constexpr char WIFI_SSID[] = "HCMUT36";
-constexpr char WIFI_PASSWORD[] = "12345679";
-constexpr char TOKEN[] = "64oj5zft2oqu20qx8zxf";
-constexpr char THINGSBOARD_SERVER[] = "app.coreiot.io";
-constexpr char TEMPERATURE_KEY[] = "temperature";
-constexpr char HUMIDITY_KEY[] = "humidity";
-constexpr uint16_t THINGSBOARD_PORT = 1883U;
-constexpr uint16_t MAX_MESSAGE_SEND_SIZE = 512U;
-constexpr uint16_t MAX_MESSAGE_RECEIVE_SIZE = 512U;
-constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
-constexpr uint64_t REQUEST_TIMEOUT_MICROSECONDS = 10000U * 1000U;
-void requestTimedOut() {
-  Serial.printf("Attribute request timed out did not receive a response in (%llu) microseconds. Ensure client is connected to the MQTT broker and that the keys actually exist on the target device\n", REQUEST_TIMEOUT_MICROSECONDS);
-}
-// Initialize underlying client, used to establish a connection
-WiFiClient espClient;
-// Initalize the Mqtt client instance
-Arduino_MQTT_Client mqttClient(espClient);
-// Initialize used apis
-OTA_Firmware_Update<> ota;
-Shared_Attribute_Update<1U, MAX_ATTRIBUTES> shared_update;
-Attribute_Request<2U, MAX_ATTRIBUTES> attr_request;
-const std::array<IAPI_Implementation*, 3U> apis = {
-    &shared_update,
-    &attr_request,
-    &ota
-};
-// Initialize ThingsBoard instance with the maximum needed buffer size
-ThingsBoard tb(mqttClient, MAX_MESSAGE_RECEIVE_SIZE, MAX_MESSAGE_SEND_SIZE, Default_Max_Stack_Size, apis);
-// Initalize the Updater client instance used to flash binary to flash memory
-Espressif_Updater<> updater;
-// Statuses for updating
-bool shared_update_subscribed = false;
-bool currentFWSent = false;
-bool updateRequestSent = false;
-bool requestedShared = false;
-void InitWiFi() {
-  Serial.println("Connecting to AP ...");
-  // Attempting to establish a connection to the given WiFi network
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    // Delay 500ms until a connection has been successfully established
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected to AP");
-}
-bool reconnect() {
-  // Check to ensure we aren't connected yet
-  const wl_status_t status = WiFi.status();
-  if (status == WL_CONNECTED) {
-    return true;
-  }
+//   Serial.println("Thẻ quét: " + uidStr);
 
-  // If we aren't establish a new connection to the given WiFi network
-  InitWiFi();
-  return true;
-}
-void update_starting_callback() {
-}
-void finished_callback(const bool & success) {
-  if (success) {
-    Serial.println("Done, Reboot now");
-    esp_restart();
-    return;
-  }
-  Serial.println("Downloading firmware failed");
-}
-void progress_callback(const size_t & current, const size_t & total) {
-  Serial.printf("Progress %.2f%%\n", static_cast<float>(current * 100U) / total);
-}
-void processSharedAttributeUpdate(const JsonObjectConst &data) {
-  //Info
-  const size_t jsonSize = Helper::Measure_Json(data);
-  char buffer[jsonSize];
-  serializeJson(data, buffer, jsonSize);
-  Serial.println(buffer);
-}
+//   // Nếu là cùng UID với lần trước => tính thời gian sử dụng
+//   if (uidStr == lastUID) {
+//     unsigned long now = millis();
+//     float durationMinutes = (now - lastUIDTime) / 60000.0;
+//     Serial.printf("UID %s đã sử dụng: %.2f phút\n", uidStr.c_str(), durationMinutes);
+//     // reset để lần sau test tiếp
+//     lastUID = "";
+//   } else {
+//     // Lưu UID & thời gian quẹt vào
+//     lastUID = uidStr;
+//     lastUIDTime = millis();
+//     Serial.println("Ghi nhận thời điểm vào...");
+//   }
+
+//   rfid.PICC_HaltA();         // Dừng đọc
+//   rfid.PCD_StopCrypto1();    // Dừng mã hóa
+// }
+
+String rfidUID = "";
+
 void setup() {
-  // Initalize serial connection for debugging
-  Serial.begin(SERIAL_DEBUG_BAUD);
-  delay(1000);
-  InitWiFi();
+  Serial.begin(115200);
+  Serial.println("Nhập UID để giả lập quét thẻ:");
 }
-void processSharedAttributeRequest(const JsonObjectConst &data) {
-  //Info
-  const size_t jsonSize = Helper::Measure_Json(data);
-  char buffer[jsonSize];
-  serializeJson(data, buffer, jsonSize);
-  Serial.println(buffer);
-}
+
 void loop() {
-  delay(1000);
-  if (!reconnect()) {
-    return;
-  }
-  if (!tb.connected()) {
-    // Reconnect to the ThingsBoard server,
-    // if a connection was disrupted or has not yet been established
-    Serial.printf("Connecting to: (%s) with token (%s)\n", THINGSBOARD_SERVER, TOKEN);
-    if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
-      Serial.println("Failed to connect");
-      return;
-    }
-    if (!requestedShared) {
-      Serial.println("Requesting shared attributes...");
-      const Attribute_Request_Callback<MAX_ATTRIBUTES> sharedCallback(&processSharedAttributeRequest, REQUEST_TIMEOUT_MICROSECONDS, &requestTimedOut, SHARED_ATTRIBUTES);
-      requestedShared = attr_request.Shared_Attributes_Request(sharedCallback);
-      if (!requestedShared) {
-        Serial.println("Failed to request shared attributes");
-      }
-    }
+  if (Serial.available()) {
+    rfidUID = Serial.readStringUntil('\n');
+    rfidUID.trim();
 
-  if (!shared_update_subscribed){
-      Serial.println("Subscribing for shared attribute updates...");
-      const Shared_Attribute_Callback<MAX_ATTRIBUTES> callback(&processSharedAttributeUpdate, SHARED_ATTRIBUTES);
-      if (!shared_update.Shared_Attributes_Subscribe(callback)) {
-      Serial.println("Failed to subscribe for shared attribute updates");
-      // continue;
-      }
-      Serial.println("Subscribe done");
-      shared_update_subscribed = true;
-    }
-  }  
-  if (!currentFWSent) {
-    currentFWSent = ota.Firmware_Send_Info(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION);
-  }
-  if (!updateRequestSent) {
-    Serial.print(CURRENT_FIRMWARE_TITLE);
-    Serial.println(CURRENT_FIRMWARE_VERSION);
-    Serial.println("Firwmare Update ...");
-    const OTA_Update_Callback callback(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION, &updater, &finished_callback, &progress_callback, &update_starting_callback, FIRMWARE_FAILURE_RETRIES, FIRMWARE_PACKET_SIZE);
-    updateRequestSent = ota.Start_Firmware_Update(callback);
-    if(updateRequestSent) {
-      delay(500);
-      Serial.println("Firwmare Update Subscription...");
-      updateRequestSent = ota.Subscribe_Firmware_Update(callback);
+    Serial.println("Thẻ được quét có UID: " + rfidUID);
+
+    // Giả lập xử lý
+    if (rfidUID == "04A1B2C3D4") {
+      Serial.println("Truy cập được cho phép.");
+    } else {
+      Serial.println("Truy cập bị từ chối.");
     }
   }
-  // Sending telemetry by time interval
-  if (millis() - previousTelemetrySend > TELEMETRY_SEND_INTERVAL)
-  {
-
-    // Use virtual random sensor
-    float temperature = random(25.8, 26);
-    float humidity = random(50.2, 51);
-
-    // Uncomment if using DHT20
-
-    // dht20.read();
-    // temperature = dht20.getTemperature();
-    // humidity = dht20.getHumidity();
-
-    // Uncomment if using DHT11/22
-    /*
-    float temperature = 0;
-    float humidity = 0;
-    dht.read2(&temperature, &humidity, NULL);
-    */
-
-    Serial.println("Sending telemetry. Temperature: " + String(temperature, 1) + " humidity: " + String(humidity, 1));
-
-    tb.sendTelemetryData(TEMPERATURE_KEY, temperature);
-    tb.sendTelemetryData(HUMIDITY_KEY, humidity);
-    tb.sendAttributeData("rssi", WiFi.RSSI()); // also update wifi signal strength
-    previousTelemetrySend = millis();
-  }
-tb.loop();
 }
